3fa3fbe12c268606882b2660dc04d801f4e25ae8
Aori Nevo <aori.nevo@bloomingdales.com>
2018-06-23 14:46:04 -0400

    I-1150: Update actions/command.js and reducers/command.js

--- actions/command.js
+++ actions/command.js
@@ -1,6 +1,7 @@
 const SET_HANDLERS = 'SET_HANDLERS'
 const SET_ALIAS_MAP = 'SET_ALIAS_MAP'
 const SET_DEFAULT_COMMAND = 'SET_DEFAULT_COMMAND'
+const ADD_HANDLER = 'ADD_HANDLER'
 const RESET_COMMAND = 'RESET_COMMAND'
 const FREEZE_COMMAND = 'FREEZE_COMMAND'
 const UNFREEZE_COMMAND = 'UNFREEZE_COMMAND'
@@ -22,6 +23,13 @@ function setAliasMap (value) {
 function setDefaultCommand (value) {
   return {
     type: SET_DEFAULT_COMMAND,
+    value
+  }
+}
+
+function addHandler (value) {
+  return {
+    type: ADD_HANDLER,
     value
   }
 }
@@ -48,12 +56,14 @@ module.exports = {
   setHandlers,
   setAliasMap,
   setDefaultCommand,
+  addHandler,
   resetCommand,
   freezeCommand,
   unfreezeCommand,
   SET_HANDLERS,
   SET_ALIAS_MAP,
   SET_DEFAULT_COMMAND,
+  ADD_HANDLER,
   RESET_COMMAND,
   FREEZE_COMMAND,
   UNFREEZE_COMMAND
